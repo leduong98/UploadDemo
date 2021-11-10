fdceb4219eae9d2fe0b90a202d0891f844fa108a
LDEV <leduong1998st@gmail.com>
2021-11-10 15:16:35 +0700

    mm

--- demo-upload/src/App.js
+++ demo-upload/src/App.js
@@ -1,29 +1,38 @@
 import 'antd/dist/antd.css';
 import {Button, message, Upload} from "antd";
 import {UploadOutlined} from '@ant-design/icons';
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
 
-  const handleUpload = (e) => {
-    let formData = new FormData();
-    formData.append("file", e.target.files[0]);
-
-    axios.post("http://localhost:8080/files", formData)
-      .then(res => {
-        console.log("res", res);
-      })
-      .catch(err => {
-        console.log("err", err);
-      });
+  const propsConfigUpload = (info) => {
+    if (info.file.status === 'done') {
+      message.success(`Success!`, 3)
+    } else if (info.file.status === 'error') {
+      message.error(`Error!`,
+        3)
+    }
   }
+
+  const handleUpload = async options =>{
+    const {onSuccess, onError, file} = options;
+    const fmData = new FormData();
+    try {
+      fmData.append("file", file);
+      const res = await axios.post(
+        "/api/candidate/uploadMaster",
+        fmData
+      );
+      onSuccess("ok")
+    } catch (err) {
+      onError({ err });
+    }
+  };
 
   return (
     <div className="App">
-      <Upload
-        onChange={handleUpload()}
-      >
-        <Button icon={<UploadOutlined/>}>Click to Upload</Button>
+      <Upload accept = ".xlsx, .xls, .pdf" customRequest={handleUpload} onChange={propsConfigUpload}>
+        <Button icon={<UploadOutlined />}>Upload Excel</Button>
       </Upload>
     </div>
   );
